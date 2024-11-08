9964af496649ebd0b783d83260467ee01241e1d3
sankalpalokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2024-11-09 04:10:52 +0900

    create files

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+
+    <!-- Favicons -->
+    <link href="img/logo.png" rel="icon">
+    <link href="img/logo.png" rel="apple-touch-icon">
+    
+    <title>Changumi Online Store</title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    
+
+<style>
+      * {
+        font-family: 'Roboto', sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .main-logo {
+        width: 150px;
+        
+    }
+
+    #topbar {
+        background: black;
+        padding: 50px;
+    }
+
+    .support-box {
+        color: white;
+    }
+
+    .cart {
+      color: white;
+    }
+</style>
+
+<header>
+      <div class="container-fluid">
+        <div id = "topbar" class="row py-3 border-bottom">
+          
+          <div class="col-sm-4 col-lg-3 text-center text-sm-start">
+            <div class="main-logo">
+              <a href="index.html">
+                <img src="img/changumiicon.png" alt="logo" class="img-fluid">
+              </a>
+            </div>
+          </div>
+          
+          <div class="col-sm-6 offset-sm-2 offset-md-0 col-lg-4 d-none d-lg-block">
+            <div class="search-bar row bg-light p-2 my-2 rounded-4">
+              <div class="col-11 col-md-10 d-none d-md-block">
+              <form id="search-form" class="text-center" action="index.html" method="post">
+                  <input type="text" class="form-control border-0 bg-transparent" placeholder="Search for more than 20,000 products" />
+                </form>
+              </div>
+              
+              <div class="col-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z"/></svg>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-sm-8 col-lg-4 d-flex justify-content-end gap-4 align-items-center mt-4 mt-sm-0 justify-content-center ">
+            <div class="support-box text-end d-none d-xl-block">
+              <span class="fs-6 text-white">For Support?</span>
+              <h5 class="mb-0">+82 10 4832 0845</h5>
+            </div>
+
+            
+
+            <div class="col-sm-6 col-lg-4 d-flex justify-content-end gap-8 align-items-center mt-4 mt-sm-0 justify-content-center justify-content-sm-end">
+						<div class="right-element">
+							<a href="#" class="user-account for-buy"><i
+									class="icon icon-user"></i><span>Account</span></a>
+							<a href="#" class="cart for-buy"><i class="icon icon-clipboard"></i><span>Cart:(0
+									$)</span></a>
+
+
+						</div>
+          </div>
+
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row py-3">
+          <div class="d-flex  justify-content-center justify-content-sm-between align-items-center">
+            <nav class="main-menu d-flex navbar navbar-expand-lg">
+
+              <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+
+                <div class="offcanvas-header justify-content-center">
+                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+
+                <div class="offcanvas-body">
+              
+                  <select class="filter-categories border-0 mb-0 me-5">
+                    <option>Shop by Departments</option>
+                    <option>Groceries</option>
+                    <option>Drinks</option>
+                    <option>Chocolates</option>
+                  </select>
+              
+                  <ul class="navbar-nav justify-content-end menu-list list-unstyled d-flex gap-md-3 mb-0">
+                    <li class="nav-item active">
+                      <a href="#women" class="nav-link">Women</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a href="#men" class="nav-link">Men</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#kids" class="nav-link">Kids</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#accessories" class="nav-link">Accessories</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" role="button" id="pages" data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
+                      <ul class="dropdown-menu" aria-labelledby="pages">
+                        <li><a href="index.html" class="dropdown-item">About Us </a></li>
+                        <li><a href="index.html" class="dropdown-item">Shop </a></li>
+                        <li><a href="index.html" class="dropdown-item">Single Product </a></li>
+                        <li><a href="index.html" class="dropdown-item">Cart </a></li>
+                        <li><a href="index.html" class="dropdown-item">Checkout </a></li>
+                        <li><a href="index.html" class="dropdown-item">Blog </a></li>
+                        <li><a href="index.html" class="dropdown-item">Single Post </a></li>
+                        <li><a href="index.html" class="dropdown-item">Styles </a></li>
+                        <li><a href="index.html" class="dropdown-item">Contact </a></li>
+                        <li><a href="index.html" class="dropdown-item">Thank You </a></li>
+                        <li><a href="index.html" class="dropdown-item">My Account </a></li>
+                        <li><a href="index.html" class="dropdown-item">404 Error </a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#brand" class="nav-link">Brand</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#sale" class="nav-link">Sale</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#blog" class="nav-link">Blog</a>
+                    </li>
+                  </ul>
+                
+                </div>
+
+              </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
